c3e979cd82b326305b2edcc5d04e2db3f5b1e0c7
lawless-m <matt-github@steponnopets.net>
2022-01-10 16:12:26 +0000

    minimal example passes

--- src/Point2DTools.jl
+++ src/Point2DTools.jl
@@ -1,5 +1,42 @@
 module Point2DTools
 
-# Write your package code here.
+using StaticArrays
 
+export Point2D, convex, convexperm
+
+const Point2D = SVector{2}
+
+Base.atan(p::Point2D) = atan(p.y, p.x)
+Base.atan(p1::Point2D, p2::Point2D) = atan(p2-p1)
+
+convex(ps::Vector{Point2D}) = ps[convexperm(ps)]
+
+function convexperm(ps::Vector{Point2D})
+    ips = sortperm(ps)
+
+    right(L, fn) =  fn(atan(ps[L[end-2]], ps[L[end-1]]), atan(ps[L[end-2]], ps[L[end]]))
+
+    Lupper = [ips[1], ips[2]]
+    
+    for i in 3:length(ips)
+        push!(Lupper, ips[i])
+        while length(Lupper) > 2 && !right(Lupper, >)
+            Lupper[end-1] = Lupper[end]
+            pop!(Lupper)
+        end
+    end
+    Lower = [ips[end], ips[end-1]]
+    for i in length(ips)-2:-1:1
+        push!(Lower, ips[i])
+        while length(Lower) > 2 && !right(Lower, <)
+            Lower[end-1] = Lower[end]
+            pop!(Lower)
+        end
+    end
+    vcat(Lupper, Lower[2:end]
+end 
+
+
+
+###
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using Point2DTools
 using Test
 
+# Computational Geometry, Third Edition, P.243 - not in order
+const hull1 = Point2D[(0.1, 0.35), (0.13, 0.22), (0.07, 0.15), (0.12, 0.3), (0.16,0.2)]
+const hull1vx = Point2D[(0.07, 0.15), (0.1, 0.35), (0.16, 0.2), (0.07, 0.15)]
+
 @testset "Point2DTools.jl" begin
-    # Write your tests here.
+    @test hull1[convex(hull1)] == hull1vx
 end
