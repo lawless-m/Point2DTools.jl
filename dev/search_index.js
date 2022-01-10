fc0986ba2bf4aafdde232e5093c551cb5ccbe052
Documenter.jl <documenter@juliadocs.github.io>
2022-01-10 13:46:48 +0000

    build based on 68dae3d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Point2DTools.jl","page":"Point2DTools.jl","title":"Point2DTools.jl","text":"","category":"section"},{"location":"","page":"Point2DTools.jl","title":"Point2DTools.jl","text":"Documentation for Point2DTools.jl","category":"page"},{"location":"","page":"Point2DTools.jl","title":"Point2DTools.jl","text":"convex","category":"page"}]
+}
